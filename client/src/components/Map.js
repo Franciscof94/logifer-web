62b6ca085a47cac25f054b6a57def80ffe7b629e
francisco <fran94_ferraro@hotmail.com>
2022-06-11 17:08:28 -0300

    change map token

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,43 +1,47 @@
 import { makeStyles } from "@material-ui/core";
-import { ImLocation2 } from 'react-icons/im'
-import Map, { Marker, NavigationControl } from 'react-map-gl';
-import {MapProvider} from 'react-map-gl';
-import 'mapbox-gl/dist/mapbox-gl.css';
-
+import { ImLocation2 } from "react-icons/im";
+import Map, { Marker, NavigationControl } from "react-map-gl";
+import { MapProvider } from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 const MapGL = () => {
-  const classes = useStyles()
-  const MAPBOX_TOKEN = 'pk.eyJ1IjoiZnJhbmNpc2NvZjk0IiwiYSI6ImNsNDl2bjZ1aDA1b2czYnBxaDZhZjdhcHAifQ.B-6IzqyCb3phLpJQzy3G4Q'
+  const classes = useStyles();
+  const MAPBOX_TOKEN =
+    "pk.eyJ1IjoiZnJhbmNpc2NvZjk0IiwiYSI6ImNsNGFiNTlmbzBibGUzY24ydHhyY3RkM3EifQ.6aFlyNcPIBUQLq7mJYIQyA";
   return (
     <MapProvider>
-       <Map
-      id="mymap"
-      initialViewState={{
-        longitude: -57.685241174415374,
-        latitude: -36.299932952694,
-        zoom: 15
-      }}
-      style={{width: 350, height: 250}}
-      mapStyle="mapbox://styles/mapbox/streets-v9"
-      mapboxAccessToken={MAPBOX_TOKEN}
-    >
-    <Marker longitude={-57.685241174415374} latitude={-36.299932952694} anchor="bottom" >
-      <ImLocation2 fontSize={35} color='#E23C31'/>
-    </Marker>
-    <NavigationControl />
-    </Map>
+      <Map
+        id="mymap"
+        initialViewState={{
+          longitude: -57.685241174415374,
+          latitude: -36.299932952694,
+          zoom: 15,
+        }}
+        style={{ width: 350, height: 250 }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxAccessToken={MAPBOX_TOKEN}
+      >
+        <Marker
+          longitude={-57.685241174415374}
+          latitude={-36.299932952694}
+          anchor="bottom"
+        >
+          <ImLocation2 fontSize={35} color="#E23C31" />
+        </Marker>
+        <NavigationControl />
+      </Map>
     </MapProvider>
   );
 };
 
 const useStyles = makeStyles((theme) => ({
-  root: { 
-    height: '250px',
-    width: '350px',
-    [theme.breakpoints.down('md')]: {
-      width: '180px',
+  root: {
+    height: "250px",
+    width: "350px",
+    [theme.breakpoints.down("md")]: {
+      width: "180px",
     },
-  }
-}))
+  },
+}));
 
-export default MapGL
+export default MapGL;
