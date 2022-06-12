21d371cee9304c499c90857b5ae8f0847c13527d
francisco <fran94_ferraro@hotmail.com>
2022-06-12 01:50:35 -0300

    map modified

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,51 +1,60 @@
-import { makeStyles } from "@material-ui/core";
-import { ImLocation2 } from "react-icons/im";
-import Map, { Marker, NavigationControl } from "react-map-gl";
-import { MapProvider } from "react-map-gl";
-import "mapbox-gl/dist/mapbox-gl.css";
-/* import mapboxgl from 'mapbox-gl/dist/mapbox-gl';
-import MapboxWorker from 'mapbox-gl/dist/mapbox-gl-csp-worker';
-mapboxgl.workerClass = MapboxWorker; */
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import { makeStyles } from '@material-ui/styles';
 
+mapboxgl.accessToken = 'pk.eyJ1IjoiZnJhbmNpc2NvZjk0IiwiYSI6ImNsNGFiNTlmbzBibGUzY24ydHhyY3RkM3EifQ.6aFlyNcPIBUQLq7mJYIQyA';
 
-const MapGL = () => {
+export default function MapGL() {
+  const classes = useStyles()
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
 
-  const MAPBOX_TOKEN =
-    "pk.eyJ1IjoiZnJhbmNpc2NvZjk0IiwiYSI6ImNsNGFiNTlmbzBibGUzY24ydHhyY3RkM3EifQ.6aFlyNcPIBUQLq7mJYIQyA";
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [lng, lat],
+      zoom: zoom
+    });
+  });
+
+  useEffect(() => {
+    if (!map.current) return; // wait for map to initialize
+    map.current.on('move', () => {
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
+    });
+  });
+
   return (
-    <MapProvider>
-      <Map
-        id="mymap"
-        initialViewState={{
-          longitude: -57.685241174415374,
-          latitude: -36.299932952694,
-          zoom: 15,
-        }}
-        style={{ width: 350, height: 250 }}
-        mapStyle="mapbox://styles/mapbox/streets-v9"
-        mapboxAccessToken={MAPBOX_TOKEN}
-      >
-        <Marker
-          longitude={-57.685241174415374}
-          latitude={-36.299932952694}
-          anchor="bottom"
-        >
-          <ImLocation2 fontSize={35} color="#E23C31" />
-        </Marker>
-        <NavigationControl />
-      </Map>
-    </MapProvider>
+    <div>
+      <div className={classes.sidebar}>
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      </div>
+      <div ref={mapContainer} className={classes.mapContainer} />
+    </div>
   );
-};
+}
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    height: "250px",
-    width: "350px",
-    [theme.breakpoints.down("md")]: {
-      width: "180px",
-    },
+  mapContainer: {
+    height: "400px"
   },
-}));
-
-export default MapGL;
+  sidebar: {
+    backgroundColor: "rgba(35, 55, 75, 0.9)",
+color: "#fff",
+padding: "6px 12px",
+fontFamily: "monospace",
+zIndex: "1",
+position: "absolute",
+top: 0,
+left: 0,
+margin: "12px",
+borderRadius: "4px",
+  }
+}))
